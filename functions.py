e0297ad63447fa661e2f38a45a73759a639ddb95
bahati <bulozealfani@gmail.com>
2023-02-09 09:51:43 -0600

    Create basic GUI

--- functions.py
+++ functions.py
@@ -8,8 +8,3 @@ def get_todo(filepath='todos.txt'):
 def write_todos(todos_args, filepath='todos.txt'):
     with open(filepath, 'w') as file_local:
         file_local.writelines(todos_args)
-
-
-# will not run in the main.py
-if __name__ == "__main__":
-    print('hello')
