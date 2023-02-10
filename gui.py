766522c768a42e9a1cf68af9004203c722450ed7
bahati <bulozealfani@gmail.com>
2023-02-10 00:20:47 -0600

    Implemented "Edit" button

--- gui.py
+++ gui.py
@@ -4,25 +4,39 @@ import PySimpleGUI as sg
 label = sg.Text("Type in a to-do")
 input_box = sg. InputText(tooltip='Enter todo', key='todo')
 add_button = sg.Button('Add')
+list_box = sg.Listbox(values=functions.get_todo(), key='todos',
+                      enable_events=True, size=(45, 10))
+edit_button = sg.Button('Edit')
 
 window = sg.Window('My To-Do App',
-                   layout=[[label], [input_box, add_button]],
+                   layout=[[label], [input_box, add_button], [list_box, edit_button]],
                    font=('Helvetica', 20))
 
 while True:
     event, values = window.read()
-    print(event)
     print(values)
-    match event:
-        case 'Add':
-            todo = values['todo'] + '\n'
+    if event == 'Add':
+        todo = values['todo'] + '\n'
 
-            todos = functions.get_todo('todos.txt')
+        todos = functions.get_todo('todos.txt')
+        todos.append(todo.title())
+        functions.write_todos(todos)
+        window['todos'].update(values=todos)
 
-            todos.append(todo.title())
+    elif event == 'Edit':
+        todo = values['todos'][0]
+        new_todo = values['todo']
 
-            functions.write_todos(todos)
-        case sg.WIN_CLOSED:
-            break
+        todos = functions.get_todo()
+        index = todos.index(todo)
+        todos[index] = new_todo.title()
+        functions.write_todos(todos)
+        window['todos'].update(values=todos)
+
+    elif event == 'todos':
+        window['todo'].update(value=values['todos'][0])
+
+    if event == sg.WIN_CLOSED:
+        break
 
 window.close()
